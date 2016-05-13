a7bd836c5449c8a48ec0ddd33dc50a4db6b3e306
omkarvijay6 <omkarvijay6@gmail.com>
2016-05-14 00:49:39 +0530

    index view logic

--- opt_site/views.py
+++ opt_site/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, RequestContext
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return render_to_response('index.html', {}, context_instance=RequestContext(request))
