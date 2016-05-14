ccc49ff3d5bafefd4f2b91662ef312d1a5547e39
omkarvijay6 <omkarvijay6@gmail.com>
2016-05-14 11:48:53 +0530

    load staticfiles and staticfiles_dirs settings

--- optimum/settings.py
+++ optimum/settings.py
@@ -73,6 +73,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'optimum.wsgi.application'
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = BASE_DIR + "staticfiles"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
@@ -115,9 +125,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
